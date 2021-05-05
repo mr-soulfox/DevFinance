6953fd3b319b3355f0a207c22201bba95e0c9285
Marcos Paulo <mr.soulfox@gmail.com>
2021-05-05 20:00:43 -0300

    Initial commit

---
+++ scripts/style.js
@@ -0,0 +1,7 @@
+//show/hide gear svg in user image
+document.getElementById('config').addEventListener('mouseover', () => {
+    document.getElementById('gearUser').style.display = 'block'
+})
+document.getElementById('config').addEventListener('mouseout', () => {
+    document.getElementById('gearUser').style.display = 'none'
+})
